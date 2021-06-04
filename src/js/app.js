a0adfcc289391bfb6aa47338d7048a887235575d
Aleksey Denisov <deoz@yandex.ru>
2021-06-04 19:18:44 +0300

    Server fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Chat from './Chat';
 
-const chat = new Chat(document.querySelector('.chat'), 'ws://localhost:7070/ws');
+const chat = new Chat(document.querySelector('.chat'), 'ws://ahj-ws-1.herokuapp.com/ws');
 chat.init();
