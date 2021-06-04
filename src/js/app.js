2c2c4c434caad088d185605126b59340892ca183
Aleksey Denisov <deoz@yandex.ru>
2021-06-04 19:34:07 +0300

    wss add

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Chat from './Chat';
 
-const chat = new Chat(document.querySelector('.chat'), 'ws://ahj-ws-1.herokuapp.com/ws');
+const chat = new Chat(document.querySelector('.chat'), 'wss://ahj-ws-1.herokuapp.com/ws');
 chat.init();
