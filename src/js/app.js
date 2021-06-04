8247ae52910c55bd521a02fe81d91c6b3dfafa42
Aleksey Denisov <deoz@yandex.ru>
2021-06-04 19:10:28 +0300

    Release

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,4 @@
-import Login from './Login';
+import Chat from './Chat';
 
-const login = new Login(document.querySelector('.chat_login'), 'ws://localhost:7070/ws');
-login.connect();
-
-// const ws = new WebSocket('ws://localhost:7070/ws');
-
-// ws.addEventListener('open', () => {
-//   console.log('connected');
-//   ws.send('hello');
-// });
-
-// ws.addEventListener('message', (evt) => {
-//   console.log(evt.data);
-// });
-
-// ws.addEventListener('close', (evt) => {
-//   console.log('connection closed', evt);
-// });
-
-// ws.addEventListener('error', () => {
-//   console.log('error');
-// });
-
-// setTimeout(() => {
-//   try {
-//     ws.send('How are you?');
-//   } catch (e) {
-//     console.log('err');
-//     console.log(e);
-//   }
-// }, 3000);
+const chat = new Chat(document.querySelector('.chat'), 'ws://localhost:7070/ws');
+chat.init();
